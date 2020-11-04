eb90c618f9110481bed311aa4a2671c42f30164d
tomi <bkc.tomi1979@gmail.com>
2020-11-04 23:12:35 +0900

    set db data and create get method

--- app/app.js
+++ app/app.js
@@ -1,9 +1,41 @@
+// api version
+const apiVer = "v1";
+// server
 const express = require("express");
-const app = express()
+const app = express();
+// database
+const sqlite3 = require("sqlite3");
+const dbPath = "db/database.sqlite3";
 
 
-app.get("/api/v1/hello", (req, res) => {
-    res.json({"message": "Hello World!"});
+
+// Get all users
+app.get(`/api/${apiVer}/users`, (req, res) => {
+    const db = new sqlite3.Database(dbPath);
+    db.all("SELECT * FROM users", (err, rows) => {
+        res.json(rows);
+    });
+    db.close();
+});
+
+// Get user
+app.get(`/api/${apiVer}/users/:id`, (req, res) => {
+    const db = new sqlite3.Database(dbPath);
+    const id = req.params.id;
+    db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
+        res.json(row);
+    });
+    db.close();
+});
+
+// Search users match keyword
+app.get(`/api/${apiVer}/search`, (req, res) => {
+    const db = new sqlite3.Database(dbPath);
+    const keyword = req.query.q;
+    db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
+        res.json(rows);
+    });
+    db.close();
 });
 
 const port = process.env.PORT || 3000;
